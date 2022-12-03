b309e1bae239def912bddcb063564f4e65be904a
albert lockett <albert.lockett@solo.io>
2022-12-02 19:28:24 -0500

    2022 day 2 part II

--- 2022/day2/src/main.rs
+++ 2022/day2/src/main.rs
@@ -16,13 +16,23 @@ fn main() {
     let r = 1; // ROCK
     let p = 2; // PAPER
     let s = 3; // SISSORS
-    let points = vec![
+    let p1_points = vec![
         // enemy ->
         //   A=r, B=p, C=s
         vec![r+d, r+l, r+w], // me = X (Rock = 1);
         vec![p+w, p+d, p+l], // me = Y (Paper = 2)
         vec![s+l, s+w, s+d], // me = Z (Sissors = 3)
     ];
+
+    let p2_points = vec![
+        // enemy ->
+        //   A=r, B=p, C=s
+        vec![l+s, l+r, l+p], // me = LOSE
+        vec![d+r, d+p, d+s], // me = DRAW
+        vec![w+p, w+s, w+r], // me = WIN
+    ];
+
+
 
     // these are the indexes in the points map ...
     let mut decryp_to_pts_idx = HashMap::new();
@@ -33,7 +43,9 @@ fn main() {
     decryp_to_pts_idx.insert("Y", 1);
     decryp_to_pts_idx.insert("Z", 2);
 
-    let mut total_points = 0;
+    let mut p1_total_points = 0;
+    let mut p2_total_points = 0;
+
     for line in strat_guide_encrypted.split("\n") {
         let mut plays = line.split(" ");
         let en_play_encrypted = plays.next().unwrap();
@@ -42,12 +54,15 @@ fn main() {
         let en_play = *decryp_to_pts_idx.get(en_play_encrypted).unwrap();
         let me_play = *decryp_to_pts_idx.get(me_play_encrypted).unwrap();
         
-        let pts = points[me_play][en_play];
-        println!("for enemy={:?} me={:?} i scored {:?}", en_play_encrypted, me_play_encrypted, pts);
-        
-        total_points += pts;
+        let p1_pts = p1_points[me_play][en_play];
+        let p2_pts = p2_points[me_play][en_play];
+        println!("P1: for enemy={:?} me={:?} i scored {:?}", en_play_encrypted, me_play_encrypted, p1_pts);
+        println!("P2: for enemy={:?} me={:?} i scored {:?}", en_play_encrypted, me_play_encrypted, p2_pts);
+        p1_total_points += p1_pts;
+        p2_total_points += p2_pts;
     }
     
-    println!("PART 1 = at the end of the elf RPS tournament I won {:?}", total_points);
+    println!("PART 1 = at the end of the elf RPS tournament I won {:?}", p1_total_points);
+    println!("PART 2 = using the real docding I won this many pts {:?}", p2_total_points);
     
 }
