2a48adaf413159a1215b6120450f55ed46486f0d
albert lockett <albert.lockett@solo.io>
2022-12-04 12:25:24 -0500

    2022 day 4 code cleanup

--- 2022/day4/main.c
+++ 2022/day4/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 
+// increment the value of some number by appending a char digit
 #define INCR(target, digit_char) \
     target = target * 10 + digit_char - '0'
 
-#define DO_CONTINIUE(continue_iter) continue_iter > 0
-#define STOP_ITER(contine_iter) *continue_iter = 0
-#define INIT_ITER_CONTINUE(contine_iter) contine_iter = 1;
+// continuing iterating standard in controlled by some flag being > 0
+#define DO_CONTINIUE(continue_iter)         continue_iter > 0
+#define STOP_ITER(contine_iter)             *continue_iter = 0
+#define INIT_ITER_CONTINUE(contine_iter)    contine_iter = 1;
 
 struct region_limits {
     u_int8_t lower;
@@ -24,19 +26,17 @@ struct elf_region_pair parse_next_pair(int* continue_iter) {
     pair.elf_one.upper = 0;
     pair.elf_two.lower = 0;
     pair.elf_two.upper = 0;
-
-    u_int8_t* struct_start = &pair;
-    int offset = 0;
-
+    int pair_offset = 0;
     char ch;
+
     while(read(0, &ch, 1) > 0){
         if ('-' == ch || ',' == ch || '-' == ch) {
-            offset++;
+            pair_offset++;
             continue;
         }
 
         if (ch >='0' && ch <= '9') {
-            INCR(struct_start[offset], ch);
+            INCR(((u_int8_t*)&pair)[pair_offset], ch);
             continue;
         }
 
@@ -47,27 +47,26 @@ struct elf_region_pair parse_next_pair(int* continue_iter) {
     STOP_ITER(continue_iter);
 }
 
-int is_region_contained(struct elf_region_pair pair) {
-    if (
-        pair.elf_one.lower <= pair.elf_two.lower && 
-        pair.elf_one.upper >= pair.elf_two.upper
-    ) {
-        return 1;
-    }
-    if (
-        pair.elf_two.lower <= pair.elf_one.lower && 
-        pair.elf_two.upper >= pair.elf_one.upper
-    ) {
+int is_limits_contained(struct region_limits r1, struct region_limits r2) {
+    if (r1.lower <= r2.lower && r1.upper >= r2.upper) {
         return 1;
     }
     return 0;
 }
 
+int is_region_contained(struct elf_region_pair pair) {
+    struct region_limits r1 = pair.elf_one;
+    struct region_limits r2 = pair.elf_two;
+    int contained = is_limits_contained(r1, r2);
+    if (contained) {
+        return contained;
+    }
+    contained += is_limits_contained(r2, r1);
+    return contained;
+}
+
 int do_limits_overlap(struct region_limits r1, struct region_limits r2) {
-    if (
-        r1.lower <= r2.lower &&
-        r2.lower <= r1.upper
-    ) {
+    if (r1.lower <= r2.lower && r2.lower <= r1.upper) {
         return 1;
     }
     return 0;
@@ -78,14 +77,12 @@ int do_regions_overlap(struct elf_region_pair pair) {
     struct region_limits r2 = pair.elf_two;
 
     int overlapping = do_limits_overlap(r1, r2);
-    if (overlapping > 0) {
-        return 1;
+    if (overlapping) {
+        return overlapping;
     }
-    overlapping = do_limits_overlap(r2, r1);
+    overlapping += do_limits_overlap(r2, r1);
     return overlapping;
 }
-
-
 
 int main(int argc, char** argv) {
     int regions_contained = 0;
