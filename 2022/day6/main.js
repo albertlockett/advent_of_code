a3ad2190323853bd1962fb84282bde1fdc77147a
albert lockett <albert.lockett@solo.io>
2022-12-06 16:47:03 -0500

    2022 day 6 javascript no destructure reduce

--- 2022/day6/main.js
+++ 2022/day6/main.js
@@ -1,15 +1,15 @@
-const len = 14;
 console.log((
+    len = 14,
     x = require('fs').readFileSync('input.txt').toString().split(''), 
-    x.reduce(([seq, found], curr, idx) => {
-        if (seq.push(curr) < len+1) return [seq, found]
+    x.reduce((seq, ch, idx) => {
+        if (seq.push(ch) < len+1) return seq
         
         seq.shift()
         for (let i = 0;   i < len -1; i++)
         for (let j = i+1; j < len;    j++) 
-            if (seq[i] == seq[j]) return [seq, found]
+            if (seq[i] == seq[j]) return seq
         
         x.splice(1)
         return idx+1
-    }, [[], -1])
+    }, [])
 ))
