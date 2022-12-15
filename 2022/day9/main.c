3caf2da51e925e55f1660040b48df1ea253aeccb
albert lockett <albert.lockett@solo.io>
2022-12-15 17:56:28 -0500

    2022 day 9 part 1

--- 2022/day9/main.c
+++ 2022/day9/main.c
@@ -6,8 +6,8 @@
 typedef struct kway_node {
   short*        values;
   short         level;
-  kway_node*    left_child;
-  kway_node*    right_child;
+  struct kway_node*    left_child;
+  struct kway_node*    right_child;
 } kway_node;
 
 typedef struct kway_tree {
@@ -16,9 +16,7 @@ typedef struct kway_tree {
   kway_node*  root;
 } kway_tree;
 
-#define EMPTY -1
-#define INSERT_SUCCESS 1
-#define NOT_INSERTED 0
+#define EMPTY (kway_node*) -1
 
 kway_tree new_kway_tree(short k) {
   kway_tree tree;
@@ -35,23 +33,24 @@ kway_node* new_kway_node(short k, short* values, short level) {
   node->right_child = EMPTY;
 
   node->values = malloc(k * sizeof(short));
-  for (int i = 0; i < k; i++) node->values[i] = values[k]; // TODO copy?
+  for (int i = 0; i < k; i++) node->values[i] = values[i];
   return node;
 }
 
 int is_same_point(short k, short* left, short* right) {
-  // TODO this only works if k = 2
-  int l = (u_int16_t*) left;
-  int r = (u_int16_t*) right;
-  return !(r & l);
+  for (int i = 0; i < k; i++) {
+    if (left[i] != right[i]) return 0;
+  }
+  return 1;
 }
 
-int compare(kway_tree* tree, kway_node* node, short* values) {
+int compare(kway_tree* tree, short* values, kway_node* node) {
   int dim = node->level % tree->k;
   return values[dim] - node->values[dim];
 }
 
-kway_node* insert(kway_tree* tree, kway_node* node, short* values, short level) {
+
+kway_node* insert_node(kway_tree* tree, kway_node* node, short* values, short level) {
   if (node == EMPTY) {
     tree->size++;
     return new_kway_node(tree->k, values, level);
@@ -61,12 +60,19 @@ kway_node* insert(kway_tree* tree, kway_node* node, short* values, short level) 
     return node;
   }
 
-  if (compare(tree, values, node->values) < 0) {
-    node->left_child = insert(tree, node->left_child, values, node->level + 1);
+  if (compare(tree, values, node) < 0) {
+    node->left_child = insert_node(tree, node->left_child, values, node->level + 1);
   } else {
-    node->right_child = insert(tree, node->right_child, values, node->level + 1);
+    node->right_child = insert_node(tree, node->right_child, values, node->level + 1);
   }
   return node;
+}
+
+void insert(kway_tree* tree, short* values) {
+  kway_node* node = insert_node(tree, tree->root, values, 0);
+  if (tree->root == EMPTY) {
+    tree->root = node;
+  }
 }
 
 #define U 'U'
@@ -75,7 +81,7 @@ kway_node* insert(kway_tree* tree, kway_node* node, short* values, short level) 
 #define R 'R'
 
 typedef struct vector {
-  short diretion;
+  char  diretion;
   short magnitude;
 } vector;
 
@@ -84,7 +90,7 @@ typedef struct vector {
 
 vector parse_next_movement() {
   vector mv;
-  mv.magnitude = -1;
+  mv.magnitude = 0;
 
   char ch;
   while (read(STDIN_FILENO, &ch, 1) > 0) {
@@ -113,38 +119,90 @@ vector parse_next_movement() {
 void move_head(short* head, vector* move) {
   switch (move->diretion) {
     case U:
-      head[0] += move->diretion;
+      head[0] += 1;
       break;
     case D:
-      head[0] -= move->diretion;
+      head[0] -= 1;
       break;
     case L:
-      head[1] += move->diretion;
+      head[1] -= 1;
       break;
     case R:
-      head[1] -= move->diretion;
+      head[1] += 1;
       break;
   }
 }
 
-void move_tail(short* head, short* tail) {
-  // TODO
+#define MOVE_N 0
+#define MOVE_Y 2
+
+
+int should_move(short* head, short* tail) {
+  int v_dist = head[0] - tail[0];
+  if (v_dist < 0) v_dist *= -1;
+
+  int h_dist = head[1] - tail[1];
+  if (h_dist < 0) h_dist *= -1;
+
+  int total_dist = v_dist + h_dist;
+
+  if (total_dist <= 1) {
+    return MOVE_N;
+  }
+
+  if (total_dist == 2) {
+    if (h_dist == v_dist == 1) {
+      return MOVE_N;
+    }
+  }
+
+  return MOVE_Y;
+}
+
+void move_tail(short *head, short* tail) {
+    int v_dist = head[0] - tail[0];
+    int h_dist = head[1] - tail[1];
+
+    if (v_dist > 0) {
+      tail[0]+=1;
+    }
+    if (v_dist < 0) {
+      tail[0] -= 1;
+    }
+    if (h_dist > 0) {
+      tail[1] += 1;
+    }
+    if (h_dist < 0) {
+      tail[1] -= 1;
+    }
 }
 
 int main() {
+  kway_tree tree = new_kway_tree(2);
+
   short head[2];
   head[0] = 0;
   head[1] = 0;
 
-  short tail_pos[2];
-  tail_pos[0] = 0;
-  tail_pos[1] = 0;
+  short tail[2];
+  tail[0] = 0;
+  tail[1] = 0;
+  insert(&tree, tail);
 
   vector move;
   do {
-    move_head(head, &move);
+    move = parse_next_movement();
+    for (int i = 0; i < move.magnitude; i++) {
+      move_head(head, &move);
+      while (should_move(head, tail)) {
+        move_tail(head, tail);
+        insert(&tree, tail);
+      }
+    }
 
   } while(move.magnitude > 0);
+
+  printf("part 1 = %d\n", tree.size);
 
   return 0;
 }
