ae21e7f4be9d4e7a1f4b55469c1e2a7e810097f8
albertlockett <albert.lockett@gmail.com>
2023-12-01 17:12:56 -0500

    2023 day 1 part 2

--- 2023/day01/normal/src/main.rs
+++ 2023/day01/normal/src/main.rs
@@ -10,8 +10,9 @@ fn main() -> std::io::Result<()> {
     let lines: u64 = contents.split("\n")
         .into_iter()
         .map(|line| {
-            print!("{:?} = {:?}\n", line, parse_num_from_line(line));
-            parse_num_from_line(line)
+            let num = parse_num_from_line_v2(line);
+            print!("{:?} = {:?}\n", line, num);
+            num
         })
         .sum();
 
@@ -32,6 +33,63 @@ fn parse_num_from_line(line: &str) -> u64 {
             }
         }
     });
+
+    match (first_digit, last_digit) {
+        (Some(first), Some(last)) => (first * 10 + last) as u64,
+        (Some(first), None) => (first * 10 + first) as u64,
+        _ => 0 as u64
+    }
+}
+
+fn parse_num_from_line_v2(line: &str) -> u64 {
+    let candidates = vec![
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+        ("zero", 0),
+        ("0", 0),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9)
+    ];
+
+
+    let mut first_start_idx = 9999999;
+    let mut first_digit = None;
+
+    let mut last_start_idx = 0;
+    let mut last_digit = None;
+
+    for (word, num) in candidates {
+        let segments: Vec<&str> = line.split(word).into_iter().collect();
+        if segments.len() > 1 {
+            let first_start = segments[0].len();
+            let last_start = line.len() - word.len() - segments[segments.len() - 1].len();
+
+            if first_start < first_start_idx {
+                first_start_idx = first_start;
+                first_digit = Some(num);
+            };
+
+            if last_start >= last_start_idx {
+                last_start_idx = last_start;
+                last_digit = Some(num);
+            };
+        }
+
+    }
 
     match (first_digit, last_digit) {
         (Some(first), Some(last)) => (first * 10 + last) as u64,
