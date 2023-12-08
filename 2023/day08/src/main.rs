50bc2c3459c48374135822e0f7b1f1487945e767
albertlockett <albert.lockett@gmail.com>
2023-12-08 16:24:54 -0500

    day 08 part 2

--- 2023/day08/src/main.rs
+++ 2023/day08/src/main.rs
@@ -14,7 +14,15 @@ impl Node {
         let l = line.chars().skip(7).take(3).collect::<String>();
         let r = line.chars().skip(12).take(3).collect::<String>();
 
-        Node {id , l, r}
+        Node { id, l, r }
+    }
+
+    fn is_p2_start(&self) -> bool {
+        self.id.chars().nth(2).unwrap() == 'A'
+    }
+
+    fn is_p2_end(&self) -> bool {
+        self.id.chars().nth(2).unwrap() == 'Z'
     }
 }
 
@@ -30,13 +38,19 @@ fn main() {
     line_iter.next(); // skip blank line
 
     let mut nodes_by_id = HashMap::<String, Node>::new();
-    line_iter
-        .map(Node::new)
-        .for_each(|node| {
-            nodes_by_id.insert(node.id.clone(), node);
-        });
+    line_iter.map(Node::new).for_each(|node| {
+        nodes_by_id.insert(node.id.clone(), node);
+    });
 
-    let mut input_iter = input_line.chars().into_iter();
+    // let p1_moves = do_part1(input_line, &nodes_by_id);
+    // println!("P1 Moves: {}", p1_moves);
+
+    let p2_moves = do_part2(input_line, &nodes_by_id);
+    println!("P2 Moves: {}", p2_moves);
+}
+
+fn do_part1(input: &str, nodes_by_id: &HashMap<String, Node>) -> u32 {
+    let mut input_iter = input.chars().into_iter();
     let mut moves = 0;
 
     let mut curr_node = "AAA";
@@ -46,14 +60,57 @@ fn main() {
             Some('L') => curr_node = &node.l,
             Some('R') => curr_node = &node.r,
             None => {
-                input_iter = input_line.chars().into_iter();
+                input_iter = input.chars().into_iter();
                 moves -= 1;
-            },
+            }
             _ => panic!("Invalid input"),
         }
         moves += 1;
     }
 
-    println!("Moves: {}", moves);
+    moves
+}
 
+fn do_part2(input: &str, nodes_by_id: &HashMap<String, Node>) -> u128 {
+    let starts = nodes_by_id
+        .values()
+        .filter(|node| node.is_p2_start())
+        .collect::<Vec<_>>();
+    let mut all_moves = vec![];
+
+    for start in starts {
+        // find number of moves for this start
+        let mut input_iter = input.chars().into_iter();
+        let mut moves = 0;
+        let mut curr_id = start.id.as_str();
+        while nodes_by_id.get(curr_id).unwrap().is_p2_end() == false {
+            let node = nodes_by_id.get(curr_id).unwrap();
+            match input_iter.next() {
+                Some('L') => curr_id = &node.l,
+                Some('R') => curr_id = &node.r,
+                None => {
+                    input_iter = input.chars().into_iter();
+                    moves -= 1;
+                }
+                _ => panic!("Invalid input"),
+            }
+            moves += 1;
+        }
+        all_moves.push(moves);
+    }
+
+    fn gcd(a: u128, b: u128) -> u128 {
+        if b == 0 {
+            a
+        } else {
+            gcd(b, a % b)
+        }
+    }
+
+    let moves = all_moves
+        .into_iter()
+        .reduce(|acc, curr| acc * curr / gcd(acc, curr))
+        .unwrap();
+
+    moves
 }
