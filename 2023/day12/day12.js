adc48a9f6373c40c2ee11de7ba3e9970daac413e
albertlockett <albert.lockett@gmail.com>
2023-12-13 21:04:01 -0500

    js solution works for input

--- 2023/day12/day12.js
+++ 2023/day12/day12.js
@@ -48,8 +48,9 @@ function get_repair_segments(line) {
 }
 
 function findAllowedLocations(target_segments, line) {
+    let farthest_child_start = line.length + 5;
     if (target_segments.length == 0) {
-        return [1, 0]
+        return [1, farthest_child_start]
     }
 
     const repair_segments = get_repair_segments(line);
@@ -64,15 +65,18 @@ function findAllowedLocations(target_segments, line) {
 
     let results = 0;
     let farthest_start = 0;
-    let farthest_child_start = line.length + 5;
-    
+
 
     for (let [offset, length] of repair_segments) {
         for (let i = 0; i < length; i++) {
-            if (offset + i + segment > farthest_child_start - 1) {
+            let offset_after_segment = offset + i + segment;
+            // if we're moving the segment into a position that the next segment
+            // must occupy, break
+            if (offset_after_segment > farthest_child_start - 1) {
                 break;
             }
-            if (segment + offset + i > line.length) {
+            // if segment would extend past end of line, break
+            if (offset_after_segment > offset + length) {
                 break;
             }
             const test_line = base_line.slice(0, offset + i) 
@@ -84,10 +88,10 @@ function findAllowedLocations(target_segments, line) {
             if (result_segs[0] == segment) {
                 let children = findAllowedLocations(
                     target_segments.slice(1),
-                    line.slice(offset + segment + 1)
+                    line.slice(offset_after_segment + 1)
                 );
                 results += children[0];
-                farthest_child_start = children[1] + offset + segment + i + 1;
+                farthest_child_start = children[1] + offset_after_segment + 1;
             } else {
                 if (results > 0) {
                     // or break?
@@ -107,23 +111,35 @@ let results = null
 line = '???..###'
 target_segments = [1, 1, 3]
 results = findAllowedLocations(target_segments, line);
+console.log("results", results, "expect 1");
 
 line = ".??..??...?##.";
 target_segments = [1, 1, 3];
 results = findAllowedLocations(target_segments, line);
-console.log("results", results);
+console.log("results", results, "expect 4");
 // expect 4?
 
-// line = "?#?#?#?#?#?#?#"
-// target_segments = [1,3,1,6]
-// results = findAllowedLocations(target_segments, line);
-// console.log("results", results);
+line = "?#?#?#?#?#?#?#"
+target_segments = [1,3,1,6]
+results = findAllowedLocations(target_segments, line);
+console.log("results", results, "expect 1");
 // expect 1
 
-const input = fs.readFileSync('input.txt', 'utf8');
+line = "?###????????"
+target_segments = [3,2,1]
+results = findAllowedLocations(target_segments, line);
+console.log("results", results, "Expect 10"); // expect 10
+
+line = "?????"
+target_segments = [2,1]
+results = findAllowedLocations(target_segments, line);
+console.log("results", results, "Ex"); // expect 3
+
+const input = fs.readFileSync('input_test1.txt', 'utf8');
 const lines = input.split('\n');
 console.log({ lines })
 
+let total = 0;
 for (let i in lines) {
     const full_line = lines[i];
     if (full_line.length == 0) {
@@ -132,6 +148,8 @@ for (let i in lines) {
     const [line, t] = full_line.split(' ');
     const target_segments = t.split(',').map(x => parseInt(x));
     const [results] = findAllowedLocations(target_segments, line);
-    console.log(line)
-    console.log("results", results);
+    console.log(`${full_line} = ${results}`)
+    total += results;
 }
+
+console.log("total", total, "expect 21");
