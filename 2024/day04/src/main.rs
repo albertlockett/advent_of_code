c47275a529f055ed3af714f8413b7a45c098d378
albertlockett <albert@lancedb.com>
2024-12-04 15:50:55 -0400

    part 1 faster

--- 2024/day04/src/main.rs
+++ 2024/day04/src/main.rs
@@ -49,20 +49,11 @@ async fn main() {
         RecordBatch::try_new(schema.clone(), vec![Arc::new(list_builder.finish())]).unwrap();
     ctx.register_batch("grid_t", batch).unwrap();
 
-    // grid.iter()
-    //     .for_each(|row| list_builder.append_value(row.iter().map(Some).collect::<Vec<_>>()));
-    // let batch =
-    //     RecordBatch::try_new(schema.clone(), vec![Arc::new(list_builder.finish())]).unwrap();
-    // ctx.register_batch("grid_t_rev", batch).unwrap();
-
-
     let tmp_view_def = "select unnest(s) as s, row_number() OVER (PARTITION BY 'a') - 1 as y";
     let coords_view_def = format!("select distinct s, y, (row_number() OVER (PARTITION BY 'a') - 1) % {} as x", dims);
     let view_defs = vec![
         format!("create view rows_tmp as {} from grid", tmp_view_def),
         format!("create view rows_tmp_rev as {} from grid_rev", tmp_view_def),
-        // format!("create view cols_tmp as {} from grid_t", tmp_view_def),
-        // format!("create view cols_tmp_rev as {} from grid_t", tmp_view_def),
         format!(
         "create view grid_coords as {} from rows_tmp order by y",
             coords_view_def
@@ -71,20 +62,8 @@ async fn main() {
             "create view grid_coords_rev as {} from rows_tmp_rev order by y",
                 coords_view_def
             ),
-        // format!(
-        //     "create view grid_t_coords as {} from cols_tmp order by y",
-        //     coords_view_def
-        // ),
-        // format!(
-        //     "create view grid_t_coords_rev as {} from cols_tmp_rev order by y",
-        //     coords_view_def
-        // ),
         "create view diag as select array_agg(s order by y asc) as s, x + y as sum from grid_coords group by x + y order by x + y".to_string(),
-        // "create view diag2 as select array_agg(s order by y desc) as s, x + y as sum from grid_t_coords group by x + y order by x + y"
-        //     .to_string(),
         "create view diag_rev as select array_agg(s order by y asc) as s, x + y as sum from grid_coords_rev group by x + y order by x + y".to_string(),
-        // "create view diag2_rev as select array_agg(s order by y desc) as s, x + y as sum from grid_t_coords_rev group by x + y order by x + y"
-        //     .to_string(),
         "create view text_fwd as \
             select array_join(s, '') as s, 'g1' as g from grid \
             union (select array_join(s, '') as s, 'gt' as g from grid_t) \
@@ -101,99 +80,12 @@ async fn main() {
     for def in view_defs {
         ctx.sql(&def).await.unwrap().collect().await.unwrap();
     }
-
-    println!("grid");
-    let result = ctx.sql(
-        // "select sum(c) from matches",
-        "select * from grid"
-    ).await.unwrap().collect().await.unwrap();
-    print_batches(&result).unwrap();
-
-    println!("gridt");
-    let result = ctx.sql(
-        // "select sum(c) from matches",
-        "select * from grid_t"
-    ).await.unwrap().collect().await.unwrap();
-    print_batches(&result).unwrap();
-
-    println!("diag");
-    let result = ctx.sql(
-        // "select sum(c) from matches",
-        "select * from diag"
-    ).await.unwrap().collect().await.unwrap();
-    print_batches(&result).unwrap();
-
-    println!("diag rev");
-    let result = ctx.sql(
-        // "select sum(c) from matches",
-        "select * from diag_rev"
-    ).await.unwrap().collect().await.unwrap();
-    print_batches(&result).unwrap();
-
-    // println!("diag 1 rev");
-    // let result = ctx.sql(
-    //     // "select sum(c) from matches",
-    //     "select * from diag1_rev"
-    // ).await.unwrap().collect().await.unwrap();
-    // print_batches(&result).unwrap();
-
-    // println!("diag 2 rev");
-    // let result = ctx.sql(
-    //     // "select sum(c) from matches",
-    //     "select * from diag2_rev"
-    // ).await.unwrap().collect().await.unwrap();
-    // print_batches(&result).unwrap();
-
-    // println!("part 1 = {:?}", result); // WHY DOESNT THIS WORK!!!!!!
-
-    println!("matches");
-    let result = ctx.sql(
-        "select * from matches",
-        // "select * from diag2"
-    ).await.unwrap().collect().await.unwrap();
-    print_batches(&result).unwrap();
-
-
     println!("part 1 not working");
     let result = ctx.sql(
-        "select sum(c) from matches",
-        // "select * from diag2"
+        "select sum(c) as part1 from matches",
     ).await.unwrap().collect().await.unwrap();
     print_batches(&result).unwrap();
 
-
-
-    // part 1
-    let mut p1_total = 0;
-    for (x_mod, y_mod) in vec![
-        (" + 1", ""),
-        (" - 1", ""),
-        ("", " + 1"),
-        ("", " - 1"),
-        (" + 1", " + 1"), // diag 2 fwd
-        (" + 1", " - 1"), //  diag1 rev
-        (" - 1", " + 1"), // diag1 fwd
-        (" - 1", " - 1"), // diag 2 rev
-    ] {
-        let uber_query = format!(
-            "
-            select g1.y, g1.x from grid_coords g1
-                inner join grid_coords g2 on g2.x = g1.x {} and g2.y = g1.y {}
-                inner join grid_coords g3 on g3.x = g2.x {} and g3.y = g2.y {}
-                inner join grid_coords g4 on g4.x = g3.x {} and g4.y = g3.y {}
-                where
-                    g1.s = 'X' and
-                    g2.s = 'M' and
-                    g3.s = 'A' and
-                    g4.s = 'S'
-            order by g1.y, g1.x
-        ",
-            x_mod, y_mod, x_mod, y_mod, x_mod, y_mod
-        );
-        let result = ctx.sql(&uber_query).await.unwrap().count().await.unwrap();
-        p1_total += result
-    }
-    println!("p1 = {}", p1_total);
 
     // part 2
     let offset_cords = vec![
