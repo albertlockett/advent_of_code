10689866e87b18ae4eee4b8ff1c61df822fcf43a
albertlockett <albert@lancedb.com>
2024-12-06 17:40:00 -0400

    day 6 part 2 wrong way

--- 2024/day06/src/lib.rs
+++ 2024/day06/src/lib.rs
@@ -57,11 +57,21 @@ pub fn doit() -> (u32, u32) {
     }
 
     let obstacles = Grid::from(rows);
+
+    // for part 1, keep track of everywhere we traversed
     let mut traversed = Grid::new(obstacles.width(), obstacles.height(), 0u8);
     traversed.set(pos_x as usize, pos_y as usize, 1);
-    let mut curr_dir = GuardDir::N;
+    
+    // for part 2
+    let mut traversed_north = Grid::new(obstacles.width(), obstacles.height(), false);
+    traversed_north.set(pos_x as usize, pos_y as usize, true);
+    fill_traversed_segment(&mut traversed_north, &obstacles, pos_x, pos_y, &GuardDir::N);
+    let mut traversed_east = Grid::new(obstacles.width(), obstacles.height(), false);
+    let mut traversed_south = Grid::new(obstacles.width(), obstacles.height(), false);
+    let mut traversed_west = Grid::new(obstacles.width(), obstacles.height(), false);
+    let mut loop_coords = vec![];
 
-    let mut traversals_dbg = 1;
+    let mut curr_dir = GuardDir::N;
 
     loop {
         let (next_x, next_y) = match curr_dir {
@@ -81,20 +91,71 @@ pub fn doit() -> (u32, u32) {
 
         // check if theres an obstacle at the next position
         if *obstacles.get(next_x as usize, next_y as usize).unwrap() == Position::Occupied {
-            curr_dir = curr_dir.turn()
-        } else {
-            pos_x = next_x;
-            pos_y = next_y;
-            traversed.set(pos_x as usize, pos_y as usize, 1);
-            traversals_dbg += 1;
+            curr_dir = curr_dir.turn();
+            continue;
         }
 
-        // debug_grid(&traversed, &obstacles, pos_x, pos_y, &curr_dir);
-        // traversed.print();
-        // println!("traversals: {}\n", traversals_dbg);
+        match curr_dir {
+            GuardDir::N => {
+                if *traversed_east.get(pos_x as usize, pos_y as usize).unwrap() {
+                    loop_coords.push((pos_x, pos_y));
+                }
+            },
+            GuardDir::E => {
+                if *traversed_south.get(pos_x as usize, pos_y as usize).unwrap() {
+                    loop_coords.push((pos_x, pos_y));
+                }
+            },
+            GuardDir::S => {
+                if *traversed_west.get(pos_x as usize, pos_y as usize).unwrap() {
+                    loop_coords.push((pos_x, pos_y));
+                }
+            },
+            GuardDir::W => {
+                if *traversed_north.get(pos_x as usize, pos_y as usize).unwrap() {
+                    loop_coords.push((pos_x, pos_y));
+                }
+            },
+        }
+
+        pos_x = next_x;
+        pos_y = next_y;
+        traversed.set(pos_x as usize, pos_y as usize, 1);
+
+        match curr_dir {
+            GuardDir::N => {
+                traversed_north.set(pos_x as usize, pos_y as usize, true);
+                fill_traversed_segment(&mut traversed_north, &obstacles, pos_x, pos_y, &curr_dir);
+                debug_grid3(&traversed_north, &obstacles, pos_x, pos_y, &curr_dir);
+                println!("");
+                // if *traversed_east.get(pos_x as usize, pos_y as usize).unwrap() {
+                //     loop_coords.push((pos_x, pos_y));
+                // }
+            },
+            GuardDir::E => {
+                traversed_east.set(pos_x as usize, pos_y as usize, true);
+                fill_traversed_segment(&mut traversed_east, &obstacles, pos_x, pos_y, &curr_dir);
+                // if *traversed_south.get(pos_x as usize, pos_y as usize).unwrap() {
+                //     loop_coords.push((pos_x, pos_y));
+                // }
+            },
+            GuardDir::S => {
+                traversed_south.set(pos_x as usize, pos_y as usize, true);
+                fill_traversed_segment(&mut traversed_south, &obstacles, pos_x, pos_y, &curr_dir);
+                // if *traversed_west.get(pos_x as usize, pos_y as usize).unwrap() {
+                //     loop_coords.push((pos_x, pos_y));
+                // }
+            },
+            GuardDir::W => {
+                traversed_west.set(pos_x as usize, pos_y as usize, true);
+                fill_traversed_segment(&mut traversed_west, &obstacles, pos_x, pos_y, &curr_dir);
+                // if *traversed_north.get(pos_x as usize, pos_y as usize).unwrap() {
+                //     loop_coords.push((pos_x, pos_y));
+                // }
+            },
+        }
+        
     }
-
-
     
     // traversed.print();
     let mut p1 = 0;
@@ -104,9 +165,70 @@ pub fn doit() -> (u32, u32) {
         }
     }
 
-    // println!("traversals: {}\n", traversals_dbg);
+    let p2 = loop_coords.len() as u32;
 
-    return (p1, 0);
+    debug_grid(&traversed, &obstacles, pos_x, pos_y, &curr_dir);
+    println!("{:?}", loop_coords);
+
+    debug_grid2(&loop_coords, &obstacles, pos_x, pos_y, &curr_dir);
+
+    return (p1, p2);
+}
+
+fn fill_traversed_segment(grid: &mut Grid<bool>, obstacles: &Grid<Position>, pos_x: i32, pos_y: i32, dir: &GuardDir) {
+    let mut pos_x = pos_x;
+    let mut pos_y = pos_y;
+    loop {
+        let (next_x, next_y) = match dir {
+            GuardDir::N => (pos_x, pos_y - 1),
+            GuardDir::E => (pos_x + 1, pos_y),
+            GuardDir::S => (pos_x, pos_y + 1),
+            GuardDir::W => (pos_x - 1, pos_y),
+        };
+
+        // check if guard went OB
+        if next_x < 0 || next_x as usize >= obstacles.width() {
+            break
+        }
+        if next_y < 0 || next_y as usize >= obstacles.height() {
+            break
+        }
+
+        // check if theres an obstacle at the next position
+        if *obstacles.get(next_x as usize, next_y as usize).unwrap() == Position::Occupied {
+            break;
+        }
+
+        pos_x = next_x;
+        pos_y = next_y;
+        grid.set(pos_x as usize, pos_y as usize, true);
+    }
+
+    loop {
+        let (next_x, next_y) = match dir {
+            GuardDir::N => (pos_x, pos_y + 1),
+            GuardDir::E => (pos_x - 1, pos_y),
+            GuardDir::S => (pos_x, pos_y - 1),
+            GuardDir::W => (pos_x + 1, pos_y),
+        };
+
+        // check if guard went OB
+        if next_x < 0 || next_x as usize >= obstacles.width() {
+            break
+        }
+        if next_y < 0 || next_y as usize >= obstacles.height() {
+            break
+        }
+
+        // check if theres an obstacle at the next position
+        if *obstacles.get(next_x as usize, next_y as usize).unwrap() == Position::Occupied {
+            break;
+        }
+
+        pos_x = next_x;
+        pos_y = next_y;
+        grid.set(pos_x as usize, pos_y as usize, true);
+    }
 }
 
 fn debug_grid(traversed: &Grid<u8>, positions: &Grid<Position>, pos_x: i32, pos_y: i32, dir: &GuardDir) {
@@ -127,6 +249,77 @@ fn debug_grid(traversed: &Grid<u8>, positions: &Grid<Position>, pos_x: i32, pos_
                 },
                 (_, _) => {}
             }
+
+            if x == pos_x as usize && y == pos_y as usize {
+                match dir {
+                    GuardDir::N => debug_grid.set(x, y, "^"),
+                    GuardDir::E => debug_grid.set(x, y, ">"),
+                    GuardDir::S => debug_grid.set(x, y, "v"),
+                    GuardDir::W => debug_grid.set(x, y, "<"),
+                }
+            }
+        }
+    }
+
+    debug_grid.print();
+}
+
+fn debug_grid2(loops: &Vec<(i32, i32)>, positions: &Grid<Position>, pos_x: i32, pos_y: i32, dir: &GuardDir) {
+    let mut debug_grid = Grid::new(positions.width(), positions.height(), ".");
+    for x in 0..positions.width() {
+        for y in 0..positions.height() {
+            let p = positions.get(x, y).unwrap_or(&Position::Empty);
+            match p {
+                Position::Occupied => {
+                    debug_grid.set(x, y, "#");
+                },
+                Position::Empty => {
+                    debug_grid.set(x, y, ".");
+                },
+            }
+
+            if x == pos_x as usize && y == pos_y as usize {
+                match dir {
+                    GuardDir::N => debug_grid.set(x, y, "^"),
+                    GuardDir::E => debug_grid.set(x, y, ">"),
+                    GuardDir::S => debug_grid.set(x, y, "v"),
+                    GuardDir::W => debug_grid.set(x, y, "<"),
+                }
+            }
+
+            for (lx, ly) in loops {
+                if x == *lx as usize && y == *ly as usize {
+                    debug_grid.set(x, y, "O");
+                }
+            }
+        }
+    }
+
+    debug_grid.print();
+}
+
+fn debug_grid3(traveresed: &Grid<bool>, obstacles: &Grid<Position>, pos_x: i32, pos_y: i32, dir: &GuardDir) {
+    let mut debug_grid = Grid::new(obstacles.width(), obstacles.height(), ".");
+    for x in 0..obstacles.width() {
+        for y in 0..obstacles.height() {
+            let t = traveresed.get(x, y).unwrap();
+            let p = obstacles.get(x, y).unwrap_or(&Position::Empty);
+            match p {
+                Position::Occupied => {
+                    debug_grid.set(x, y, "#");
+                },
+                Position::Empty => {
+                    debug_grid.set(x, y, ".");
+                },
+            }
+            match t {
+                true => {
+                    debug_grid.set(x, y, "X");
+                },
+                false => {}
+            }
+
+            
 
             if x == pos_x as usize && y == pos_y as usize {
                 match dir {
--- 2024/day06/src/main.rs
+++ 2024/day06/src/main.rs
@@ -2,6 +2,7 @@
 use day06::doit;
 
 fn main() {
-    let (p1, _) = doit();
-    println!("p1 = {}", p1);
+    let (p1, p2) = doit();
+    println!("p1 = {} = 4647", p1);
+    println!("p2 = {} = ", p2);
 }
