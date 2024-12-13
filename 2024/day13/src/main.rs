364031aa2c7ea25894a3dcb161df6801497f54e5
albertlockett <albert@lancedb.com>
2024-12-13 08:23:09 -0500

    day 13 part 2

--- 2024/day13/src/main.rs
+++ 2024/day13/src/main.rs
@@ -4,7 +4,8 @@ fn main() {
 
     let games = input.split("\n\n");
 
-    let mut total_tokens = 0.0;
+    let mut p1_tokens = 0.0;
+    let mut p2_tokens = 0.0;
     for game in games {
         println!("\n{}", game);
         let lines = game.split("\n").collect::<Vec<_>>();
@@ -19,27 +20,44 @@ fn main() {
         let xc = parse_prize_num(prize.get(1).unwrap());
         let yc = parse_prize_num(prize.get(2).unwrap());
 
+        // part 1 = 
         let b = (-1.0 * xa * yc / (xb * ya) + xc / xb) / (1.0 - xa * yb / (xb * ya));
         let a = (-1.0 * b * yb + yc) / ya;
-        println!("a = {}\nb={}", a, b);
-
-        if !is_basically_integer(b) || !is_basically_integer(a) {
-            println!("No winners");
-            continue;
+        
+        if is_basically_integer(b) && is_basically_integer(a) {
+            p1_tokens += 3.0 * a + 1.0 * b;
         }
 
-        let tokens = 3.0 * a + 1.0 * b;
-        println!("tokens = {}", tokens);
+        // part 2
+        let gcfs = gcfs(xc as u64 + 10000000000000, yc as u64 + 10000000000000);
 
-        total_tokens += tokens;
+        for factor in gcfs {
+            let factor = factor as f64;
+            let yc = (yc + 10000000000000.0) / factor;
+            let xc = (xc + 10000000000000.0) / factor;
+
+
+            let b = factor * (-1.0 * xa * yc / (xb * ya) + xc / xb) / (1.0 - xa * yb / (xb * ya));
+            let a = factor * (-1.0 * b * yb + yc) / ya;
+
+            if !is_basically_integer(b) || !is_basically_integer(a) {
+                continue;
+            }
+
+            let tokens = 3.0 * a + 1.0 * b;
+            p2_tokens += tokens;
+            break;
+        }
     }
 
-    println!("\n~~~~\np1 = {}", total_tokens);
+    println!("\n~~~~\np1 = {}\np2 = {}", p1_tokens, p2_tokens);
+    assert_eq!(p1_tokens, 36870.0);
+    assert_eq!(p2_tokens, 78101482023732.0);
 }
 
 fn is_basically_integer(f: f64) -> bool {
     let m = f % 1.0;
-    let threshold = 0.0001;
+    let threshold = 0.001;
     if m < threshold {
         return true
     }
@@ -57,4 +75,30 @@ fn parse_button_num(s: &str) -> f64 {
 
 fn parse_prize_num(s: &str) -> f64 {
     s.split("=").last().unwrap().trim_end_matches(",").parse().unwrap()
+}
+
+
+fn gcfs(x: u64, y: u64) -> Vec<u64> {
+    let mut result = vec![1];
+
+    let gcd = gcd(x, y);
+
+    for i in 1..((gcd as f64).sqrt().ceil() as u64) {
+        if gcd % i == 0 {
+            result.push(i);
+            result.push(gcd / i);
+        }
+    }
+
+    result
+}
+
+fn gcd(mut x: u64, mut y: u64) -> u64 {
+    while y > 0 {
+        let tmp = x % y;
+        x = y;
+        y = tmp;
+    }
+
+    x
 }
