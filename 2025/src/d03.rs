056b7287493fef7c980ac0a7d137e1bc47729b06
albertlockett <albert.lockett@gmail.com>
2025-12-03 18:16:12 -0500

    cleanup day 3

--- 2025/src/d03.rs
+++ 2025/src/d03.rs
@@ -5,6 +5,15 @@ use crate::Challenge;
 #[derive(Default)]
 pub struct Day03 {}
 
+impl Day03 {
+    fn do_it(input: &str, digs: usize) -> Result<usize> {
+        Self::read_input_iter(input)?
+            .split(b'\n')
+            .map(|line| line.map(|nums| calc_biggest(&nums, digs)))
+            .sum()
+    }
+}
+
 fn find_max_and_pos(nums: &[u8]) -> (usize, usize) {
     let mut max = 0;
     let mut pos = 0;
@@ -18,7 +27,7 @@ fn find_max_and_pos(nums: &[u8]) -> (usize, usize) {
     (pos, (max - b'0') as usize)
 }
 
-fn do_it(nums: &[u8], digs: usize) -> usize {
+fn calc_biggest(nums: &[u8], digs: usize) -> usize {
     let mut next_pos = 0;
     let mut total = 0;
     for i in 0..digs {
@@ -34,29 +43,11 @@ fn do_it(nums: &[u8], digs: usize) -> usize {
 
 impl Challenge for Day03 {
     fn do_p1(&mut self, input: &str) -> Result<usize> {
-        let mut sum_biggest = 0;
-        let lines = Self::read_input_iter(input)?.split(b'\n');
-
-        for line in lines {
-            let nums = line?;
-            let biggest = do_it(&nums, 2);
-            sum_biggest += biggest
-        }
-
-        Ok(sum_biggest)
+        Self::do_it(input, 2)
     }
 
     fn do_p2(&mut self, input: &str) -> Result<usize> {
-        let mut sum_biggest = 0;
-        let lines = Self::read_input_iter(input)?.split(b'\n');
-
-        for line in lines {
-            let nums = line?;
-            let biggest = do_it(&nums, 12);
-            sum_biggest += biggest;
-        }
-
-        Ok(sum_biggest)
+        Self::do_it(input, 12)
     }
 }
 
@@ -66,8 +57,8 @@ mod test {
 
     #[test]
     fn test_do_it() {
-        assert_eq!(do_it(b"987654321111111", 12), 987654321111);
-        assert_eq!(do_it(b"811111111111119", 12), 811111111119);
-        assert_eq!(do_it(b"234234234234278", 12), 434234234278);
+        assert_eq!(calc_biggest(b"987654321111111", 12), 987654321111);
+        assert_eq!(calc_biggest(b"811111111111119", 12), 811111111119);
+        assert_eq!(calc_biggest(b"234234234234278", 12), 434234234278);
     }
 }
